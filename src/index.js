06ee29a52cdca21e32246be8d753e10ba0dd1717
yaroslavklimuk21 <yaroslavklimuk21@gmail.com>
2020-09-29 12:53:28 +0300

    init: start typical arrays problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  if ( !array || array.length === 0){
+    return 0;
+  }
+  let min = Math.min(...array);
+    return min;
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+  if ( !array || array.length === 0){
+    return 0;
+  }
+  let min = Math.max(...array);
+    return min;
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  if ( !array || array.length === 0){
+    return 0;
+  }
+  let sum = 0;
+  for ( let i = 0; i < array.length; i++){
+    sum += array[i];
+  }
+  let avg = sum / array.length;
+  return avg;
+};
